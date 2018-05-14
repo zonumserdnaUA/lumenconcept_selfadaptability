cab2c35ecf23cb6115359752ea18ee002eb82e04
Andres Munoz Uniandes <af.munoz11@uniandes.edu.co>
2018-05-13 23:07:19 -0500

    Removed unnecessary data

--- app/db/DBKeys.js
+++ app/db/DBKeys.js
@@ -6,13 +6,11 @@ exports.getSecretAccessKey = getSecretAccessKey;
 // private methods
 
 function getAccessKeyId() {
-    // var accessKey = process.env.DB_ACCESS_KEY_ID;
-    var accessKey = "AKIAJVRDUP3ITIHBJB5Q";
+    var accessKey = process.env.DB_ACCESS_KEY_ID;
     return accessKey;
 }
 
 function getSecretAccessKey() {
-    // var secretAccessKey = process.env.DB_SECRET_ACCESS_KEY;
-    var secretAccessKey = "dx8dh4y84H/MlXPPCdGLEr+cHEKvYEGgioS88EWz";
+    var secretAccessKey = process.env.DB_SECRET_ACCESS_KEY;
     return secretAccessKey;
 }
