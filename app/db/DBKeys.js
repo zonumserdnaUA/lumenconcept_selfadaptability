b5829498ad98e0507af3ae4542dbf44646e8ef42
Andres Munoz Uniandes <af.munoz11@uniandes.edu.co>
2018-05-19 00:23:45 -0500

    Billing Fraud lambda service integration

--- app/db/DBKeys.js
+++ app/db/DBKeys.js
@@ -6,11 +6,13 @@ exports.getSecretAccessKey = getSecretAccessKey;
 // private methods
 
 function getAccessKeyId() {
-    var accessKey = process.env.DB_ACCESS_KEY_ID;
+    // var accessKey = process.env.DB_ACCESS_KEY_ID;
+    var accessKey = "AKIAJVRDUP3ITIHBJB5Q";
     return accessKey;
 }
 
 function getSecretAccessKey() {
-    var secretAccessKey = process.env.DB_SECRET_ACCESS_KEY;
+    // var secretAccessKey = process.env.DB_SECRET_ACCESS_KEY;
+    var secretAccessKey = "dx8dh4y84H/MlXPPCdGLEr+cHEKvYEGgioS88EWz";
     return secretAccessKey;
 }
